9a060a3b2ef431cc49f292b33a2ff141bc8f4152
Daniela <Daniela@192.168.1.10>
2015-08-27 13:37:37 -0600

    Driver

--- HDT6/src/hdt6/HDT6.java
+++ HDT6/src/hdt6/HDT6.java
@@ -6,6 +6,7 @@
 
 package hdt6;
 
+import java.util.Scanner;
 /**
  *
  * @author Daniela
@@ -16,7 +17,11 @@ public class HDT6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Bienvenido al programa.\nPor favor ingrese el número de personas que van a pertenecer a los conjuntos:");
+        int cantpersonas = escaner.nextInt();
+        System.out.println("Ingrese la implementacion que desea utilizar.\n1.HashSet 2.TreeSet 3.LinkedHashSet");
+        int implementacion = escaner.nextInt();
     }
     
 }
