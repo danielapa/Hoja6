a38bb0f784e483a1a102719533c8133ac1491649
Daniela <Daniela@192.168.1.2>
2015-08-27 18:12:56 -0600

    Factory

--- HDT6/src/hdt6/DriverSet.java
+++ HDT6/src/hdt6/DriverSet.java
@@ -29,15 +29,19 @@ public class DriverSet {
         Scanner escaner = new Scanner(System.in);
         System.out.println("Bienvenido al programa.\nPor favor ingrese el número de personas que van a pertenecer a los conjuntos:");
         int cantpersonas = escaner.nextInt();
+        String[] nombres = new String[cantpersonas];
         System.out.println("Ingrese la implementacion que desea utilizar.\n1.HashSet 2.TreeSet 3.LinkedHashSet");
         int implementacion = escaner.nextInt();        
         
         for(int x=0;x<cantpersonas;x++){
             System.out.println("Ingrese el nombre de la persona: ");
-            String nombre = escaner.next();
-            hs1.add(nombre);
-            if(hs1.contains(nombre)){
-                contador++;
+            nombres[x] = escaner.next();
+            hs1.add(nombres[x]);
+        }
+        
+        for(int y=0; y<cantpersonas;y++){
+            if(hs1.contains(nombres[y])){
+                    contador++;
             }
         }
         
--- HDT6/src/hdt6/SetFactory.java
+++ HDT6/src/hdt6/SetFactory.java
@@ -8,10 +8,52 @@
 
 package hdt6;
 
+import java.util.*;
+
 /**
  *
  * @author Daniela
  */
 public class SetFactory {
+    public Set SetGrupos1(int implementacion){
+        Set<String> hs1=null;
+        if(implementacion==1){
+            hs1 = new HashSet<String>();
+        }
+        if(implementacion==2){
+            hs1 = new TreeSet<String>();
+        }
+        if(implementacion==3){
+            hs1 = new LinkedHashSet<String>();
+        }
+        return hs1;
+    }
     
+    public Set SetGrupos2(int implementacion){
+        Set<String> hs2=null;
+        if(implementacion==1){
+            hs2 = new HashSet<String>();
+        }
+        if(implementacion==2){
+            hs2 = new TreeSet<String>();
+        }
+        if(implementacion==3){
+            hs2 = new LinkedHashSet<String>();
+        }
+        return hs2;
+    }
+    
+    public Set SetGrupos3(int implementacion){
+        Set<String> hs3=null;
+        if(implementacion==1){
+            hs3 = new HashSet<String>();
+        }
+        if(implementacion==2){
+            hs3 = new TreeSet<String>();
+        }
+        if(implementacion==3){
+            hs3 = new LinkedHashSet<String>();
+        }
+        return hs3;
+    }
 }
